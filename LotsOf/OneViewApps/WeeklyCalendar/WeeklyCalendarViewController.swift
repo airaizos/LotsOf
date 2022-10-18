b755eefc41d6ee29ce05516221d62e788b2092b1
adrian <adrian@iraizos.me>
2022-10-18 12:10:28 +0200

    collectionDataSource done!

--- LotsOf/OneViewApps/WeeklyCalendar/WeeklyCalendarViewController.swift
+++ LotsOf/OneViewApps/WeeklyCalendar/WeeklyCalendarViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 final class WeeklyCalendarViewController: UIViewController {
 
     @IBOutlet weak var monthLabel: UILabel!
-    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var selectedDate = Date()
+    var totalSquares = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
        
+    }
+    
+    //FUNC
+    
+    func setCellsView() {
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+        
     }
     
 
@@ -32,4 +44,21 @@ final class WeeklyCalendarViewController: UIViewController {
         print("    [DEINIT] ->      WeeklyCalendarViewController ViewController")
     }
 
+}
+
+
+extension WeeklyCalendarViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquares.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        cell.dayOfMonth.text = totalSquares[indexPath.row]
+        return cell
+        
+    }
+    
+
+    
 }

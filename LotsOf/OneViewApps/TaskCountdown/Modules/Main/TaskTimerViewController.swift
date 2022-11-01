6940716b1697383fc2574f87e8afedd31902ffb1
adrian <adrian@iraizos.me>
2022-11-01 19:52:57 +0100

    TastkTimerVC

---
+++ LotsOf/OneViewApps/TaskCountdown/Modules/Main/TaskTimerViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TaskTimerViewController.swift
+//  LotsOfUrlSessions
+//
+//  Created by Adrian Iraizos Mendoza on 18/10/22.
+//
+
+import UIKit
+
+final class TaskTimerViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    // MARK: - Variables
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Outlets & objc functions
+    
+    // MARK: - Functions
+    
+
+
+}

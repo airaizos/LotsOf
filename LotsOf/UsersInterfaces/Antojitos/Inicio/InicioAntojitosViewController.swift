dddb54f2ef8649783e006db6d18a8a9451cac604
adrian <adrian@iraizos.me>
2022-11-11 12:09:17 +0100

    vC embedded Tab and navigation

--- LotsOf/UsersInterfaces/Antojitos/Inicio/InicioAntojitosViewController.swift
+++ LotsOf/UsersInterfaces/Antojitos/Inicio/InicioAntojitosViewController.swift
@@ -8,21 +8,46 @@
 import UIKit
 
 final class InicioAntojitosViewController: UIViewController {
-
-    @IBOutlet weak var inicioScrollView: InicioScrollView!
     
+    //MARK: Variables
+    let antojitosTabBarController = UITabBarController()
+//    let inicioVC = InicioAntojitosViewController()
+    let promocionesVC = PromocionesAntojitosViewController()
+    let cartaVC = CartaAntojitosViewController()
+    let miAntojitosVC = MiAntojitosViewController()
+    let cestaVC = CestaAntojitosViewController()
+   
+    
+    
+    //MARK: Outlets
     @IBOutlet weak var promotionalCodeView: PromotionalCodeView!
     
+    //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       setupView()
+        setupView()
     }
     
+    //MARK: Functions
     func setupView() {
         promotionalCodeView.setup(presentationController: self)
     }
+    
+    func setupTabBarController() {
+//          inicioVC.tabBarItem = UITabBarItem(title: "Inicio", image: UIImage(systemName: "house.fill"), tag: 0)
+          promocionesVC.tabBarItem = UITabBarItem(title: "Promociones", image: UIImage(systemName: "eurosign.circle"), tag: 1)
+          cartaVC.tabBarItem = UITabBarItem(title: "Carta", image: UIImage(systemName: "doc.plaintext"), tag: 2)
+          miAntojitosVC.tabBarItem = UITabBarItem(title: "Mi Antojitos", image: UIImage(systemName: "heart.circle"), tag: 3)
+          cestaVC.tabBarItem = UITabBarItem(title: "Cesta", image: UIImage(systemName: "heart.circle"), tag: 4)
+          
+          let controllers = [ promocionesVC, cartaVC, miAntojitosVC, cestaVC]
+          
+          antojitosTabBarController.viewControllers = controllers
+          
+          antojitosTabBarController.viewControllers = controllers.map { UINavigationController(rootViewController: $0) }
+      }
 
+    
     deinit {
         print("    [DEINIT] ->      ANTOJITOSINICIO ViewController")
     }

53e96f073ad178a83a78826e72cd44357a307f47
adrian <adrian@iraizos.me>
2022-11-23 23:07:53 +0100

    groupCell

--- LotsOf/DataPersistance/QatarWorldCup2022/Groups/GroupCell/GroupCell.swift
+++ LotsOf/DataPersistance/QatarWorldCup2022/Groups/GroupCell/GroupCell.swift
@@ -9,6 +9,46 @@ import UIKit
 
 final class GroupCell: UICollectionViewCell {
     
+    @IBOutlet weak var flagImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var winsLabel: UILabel!
+    @IBOutlet weak var lossesLabel: UILabel!
+    @IBOutlet weak var drawsLabel: UILabel!
+    @IBOutlet weak var goalsForLabel: UILabel!
+    @IBOutlet weak var goalsAgainst: UILabel!
+    @IBOutlet weak var goalsDifference: UILabel!
+    
+    func configure(with model: GroupCellModel) {
+        flagImage.loadImageUsingCache(withUrl: model.flag)
+        nameLabel.text = model.name
+        pointsLabel.text = "\(model.points)"
+        winsLabel.text = "\(model.wins)"
+        lossesLabel.text = "\(model.losses)"
+        drawsLabel.text = "\(model.draws)"
+        goalsForLabel.text = "\(model.goalsFor)"
+        goalsAgainst.text = "\(model.goalsAgainst)"
+        goalsDifference.text = "\(model.goalsDifference)"
+    }
+    
+}
+
+struct GroupCellModel {
+    let flag: String
+    let name: String
+    var points: Int {
+        return (wins * 3) + draws
+    }
+    let wins: Int
+    let losses: Int
+    let draws: Int
+    let goalsFor: Int
+    let goalsAgainst: Int
+    var goalsDifference: Int {
+        return goalsFor - goalsAgainst
+    }
+    
+    
     
     
 }

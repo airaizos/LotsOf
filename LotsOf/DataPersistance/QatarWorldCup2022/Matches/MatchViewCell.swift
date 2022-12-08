d03eb551f9b60e09192501e7c310969144464935
adrian <adrian@iraizos.me>
2022-12-08 19:29:30 +0100

    Added MatchCell

---
+++ LotsOf/DataPersistance/QatarWorldCup2022/Matches/MatchViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MatchViewCell.swift
+//  LotsOfUrlSessions
+//
+//  Created by Adrian Iraizos Mendoza on 8/12/22.
+//971c45
+
+import UIKit
+
+final class MatchViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ LotsOf/DataPersistance/QatarWorldCup2022/Matches/QatarMatchesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QatarMatchesViewController.swift
+//  LotsOfUrlSessions
+//
+//  Created by Adrian Iraizos Mendoza on 8/12/22.
+//
+
+import UIKit
+
+final class QatarMatchesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
